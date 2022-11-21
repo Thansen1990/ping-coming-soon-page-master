f08563426300c77dca1661303e6e3ad1d7dd7b87
Thansen1990 <THansen1990@live.com>
2022-11-20 16:12:52 -0800

    Fixes to error/acceptance message

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const submit_email = document.querySelector('.submit_button');
+const email_input = document.querySelector('.email_input')
+const email_error_message = document.querySelector('.email_error_message')
+const email_accept_message = document.querySelector('.email_accept_message')
 
 
 
@@ -7,11 +10,19 @@ submit_email.addEventListener('click', function validateForm() {
     var formdata = document.getElementById('email').value;// grab form data
     const regexTest = emailRegex.test(formdata); //tests regex again formdata
 
+    email_error_message.classList.remove('error_display')
+    email_input.classList.remove('email_error_border')
+
     if (regexTest === false){
-        alert('Please enter a valid email');
+        email_accept_message.classList.remove('error_display')
+        email_error_message.classList.add('error_display')
+        email_input.classList.add('email_error_border')
         return;
     } else {
-        alert('Your e-mail has been submitted');
+        email_error_message.classList.remove('error_display')
+        email_input.classList.remove('email_error_border')
+        document.getElementById('email_form').reset();
+        email_accept_message.classList.add('error_display')
         return;
     };
 });
